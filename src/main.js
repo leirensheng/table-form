32d6c34ae3c02ad2da00d2232370e8402a7455e1
leirensheng <leirensheng@163.com>
2019-10-14 16:03:48 +0800

    feat: elementui按需引入

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 import Vue from "vue";
 import App from "./App.vue";
-// import { Button, Select, Table } from "element-ui";
-import Element from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import {
+  Button,
+  Select,
+  Table,
+  Pagination,
+  Input,
+  TableColumn,
+  FormItem,
+  Loading,
+  Dialog,
+  Form,
+  Option,
+  Upload,
+  Message
+} from "element-ui";
+// import Element from "element-ui";
+// import "element-ui/lib/theme-chalk/index.css";
 import fetch from "@/fetch";
 
-Vue.use(Element);
-// Vue.use(Select);
-// Vue.use(Table);
+// Vue.use(Element);
+Vue.use(Select);
+Vue.use(Table);
+Vue.use(Button);
+Vue.use(Pagination);
+Vue.use(Input);
+Vue.use(TableColumn);
+Vue.use(Pagination);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Dialog);
+Vue.use(Option);
+Vue.use(Upload);
+Vue.use(FormItem);
+
+Vue.use(Loading.directive);
+Vue.prototype.$message = Message;
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = fetch;
